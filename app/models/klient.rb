5d3bf536259bb8625c695f1f50a614d3e03bc231
Kamil Pek <kamil.pek@gmail.com>
2016-11-26 12:17:06 +0100

    uruchomienie zgloszen

--- app/models/klient.rb
+++ app/models/klient.rb
@@ -1,4 +1,4 @@
 class Klient < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
   has_many :zgloszenie
 end
--- app/models/zgloszenie.rb
+++ app/models/zgloszenie.rb
@@ -1,6 +1,6 @@
 class Zgloszenie < ApplicationRecord
-  belongs_to :klient
-  belongs_to :pracownik
-  belongs_to :dzial
-  has_many :zgloszenie_aktywnosc
+  # belongs_to :klient
+  # belongs_to :pracownik
+  # belongs_to :dzial
+  # has_many :zgloszenie_aktywnosc
 end
