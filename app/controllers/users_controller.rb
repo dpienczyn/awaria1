632ea88c4c87dea312d445ffa2fd95c3bc792e31
Kamil Pek <kamil.pek@gmail.com>
2016-12-04 21:21:40 +0100

    usuwanie uzytkownikow i nadawanie uprawnien administrator

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,27 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  # GET /paliwkos/1
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    if @user.destroy
+        redirect_to users_path, notice: "Użytkownik usunięty."
+    end
+  end
+
   def grantadmin
-    # id = User.where(id:@user.id).pluck(:id).last
-    # user = User.find(id)
-    User.find(@users.id)
+    @user = User.find(params[:id])
+    user = User.find(@user.id)
     user.update_attribute(:admin, true)
+    redirect_to users_path, notice: "Nadano uprawnienia administratora."
+  end
+
+  def denyadmin
+    @user = User.find(params[:id])
+    user = User.find(@user.id)
+    user.update_attribute(:admin, false)
+    redirect_to users_path, notice: "Odebrano uprawnienia administratora."
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,8 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
 
   match 'users/:id' => 'users#destroy', :via => :delete, :as => :admin_destroy_user
-  match 'users/:id' => 'users#grantadmin', :via => :patch, :as => :admin_grant_admin_user
+  match 'users/:id' => 'users#grantadmin', :via => :post, :as => :admin_grant_admin_user
+  match 'users/:id' => 'users#denyadmin', :via => :post, :as => :admin_deny_admin_user
 
   root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
