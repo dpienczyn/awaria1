a21533d7569f23393699d06b0a59a9c961a8f06d
Marcin Dawidowski <mdawidowski@sigma.uge.du.pl>
2016-12-09 23:19:23 +0100

    nic nowego

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161209203829) do
+ActiveRecord::Schema.define(version: 20161209215252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,8 @@ ActiveRecord::Schema.define(version: 20161209203829) do
     t.string   "first_name"
     t.string   "last_name"
     t.integer  "dzial_id"
+    t.boolean  "email_confirmed",        default: false
+    t.string   "confirm_token"
     t.index ["dzial_id"], name: "index_users_on_dzial_id", using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
