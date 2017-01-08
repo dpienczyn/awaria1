a5bb9d71a57b862907ff0129c523f16a924dddd4
Kamil Pek <kamil.pek@gmail.com>
2017-01-08 11:15:35 +0100

    poprawki w nawigacji

--- config/routes.rb
+++ config/routes.rb
@@ -35,6 +35,17 @@ Rails.application.routes.draw do
     end
   end
 
-  root 'static_pages#home'
+  devise_scope :user do
+    authenticated :user do
+      root 'zgloszenies#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
+  # root 'static_pages#home'
+  # root 'devise/sessions'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
