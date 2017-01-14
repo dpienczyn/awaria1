a48cfafa66fcfeb03363c45347b5ff530c33328f
Kamil Pek <kamil.pek@gmail.com>
2017-01-14 15:42:26 +0100

    zabezpieczenie pdf

--- app/controllers/zgloszenie_lista_pdf.rb
+++ app/controllers/zgloszenie_lista_pdf.rb
@@ -1,15 +1,16 @@
 class ZgloszenieListaPdf < Prawn::Document
 
-  def initialize(zgloszenies)
+  def initialize(zgloszenies, user)
     super()
     @zgloszenies = zgloszenies
+    @user = user
     zgloszenie_header
     zgloszenie_lines
   end
 
   def zgloszenie_header
-    font("app/assets/fonts/SourceSansPro-Regular.ttf", size: 30) do
-      text "Lista Zgłoszeń"
+    font("app/assets/fonts/SourceSansPro-Regular.ttf", size: 24) do
+      text "Lista Zgłoszeń użytkownika: #{User.where(id:@user).pluck(:first_name).last} #{User.where(id:@user).pluck(:last_name).last}."
     end
   end
 
@@ -28,9 +29,15 @@ class ZgloszenieListaPdf < Prawn::Document
   def zgloszenie_lines_rows
     index = 0
     [["LP", "Dział", "Priorytet", "Status", "Data zgłoszenia", "Data naprawy", "Nazwa Urządzenia", "Opis Uszkodzenia"]] +
-    @zgloszenies.map do |zgloszenie|
+    @zgloszenies.select{|a| a.user_id == @user}.map do |zgloszenie|
       index += 1
-      [index, Dzial.where(id:zgloszenie.dzial_id).pluck(:nazwa).last, zgloszenie.priorytet.to_s, zgloszenie.status.to_s, zgloszenie.data_zgloszenia.to_s, zgloszenie.data_naprawy.to_s, zgloszenie.nazwa_urzadzenia, zgloszenie.opis_uszkodzenia]
+      [index,
+        Dzial.where(id:zgloszenie.dzial_id).pluck(:nazwa).last,
+        zgloszenie.priorytet.to_s, zgloszenie.status.to_s,
+        zgloszenie.data_zgloszenia.to_s,
+        zgloszenie.data_naprawy.to_s,
+        zgloszenie.nazwa_urzadzenia,
+        zgloszenie.opis_uszkodzenia]
     end
   end
 end
