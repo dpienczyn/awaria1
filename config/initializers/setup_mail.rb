bc35cdbe7f0e85f6aa923676cf88e1e288d1b536
dpienczyn <donia19881@wp.pl>
2017-01-28 00:55:16 +0100

    domain

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :domain               => "localhost:3000",
+  :domain               => "http://awaria1.herokuapp.com/",
   :user_name            => "awaria.kontakt@gmail.com",
   :password             => "admin1234567",
   :authentication       => "plain",
