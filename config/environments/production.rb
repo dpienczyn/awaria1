7824d853750f01f20a6fe53ef8ee65ca04f53a97
dpienczyn <donia19881@wp.pl>
2017-01-28 16:57:38 +0100

    w

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,12 +90,11 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => 'gmail.com',
+    :domain               => 'awaria1.herokuapp.com',
     :user_name            => "awaria.kontakt@gmail.com",
     :password             => "admin1234567",
     :authentication       => "plain",
-    :enable_starttls_auto => true,
-    :openssl_verify_mode => 'none'
+    :enable_starttls_auto => true
   }
 
 end
