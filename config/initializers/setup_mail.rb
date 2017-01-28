37bf2779609c62f8c04d155db04df88066676cc8
dpienczyn <donia19881@wp.pl>
2017-01-28 13:33:17 +0100

    zmiany

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,6 +1,6 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
-  :port                 => 587,
+  :port                 => 465,
   :user_name            => "awaria.kontakt@gmail.com",
   :password             => "admin1234567",
   :authentication       => "plain",
