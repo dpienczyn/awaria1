92eb7bb1a401c1bc1956f3fa6cd98cf39406813b
dpienczyn <donia19881@wp.pl>
2017-05-08 16:01:10 +0200

    kompresja

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,11 @@ Bundler.require(*Rails.groups)
 
 module Awaria
   class Application < Rails::Application
+  	config.middleware.use Rack::Deflater
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.exceptions_app = self.routes
+
+    config.cache_store = :memory_store
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
